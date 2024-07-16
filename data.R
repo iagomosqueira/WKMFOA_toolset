2c4805ca023b093a1ae456a11ad6ccb8705998ed
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2024-07-16 09:24:05 +0200

    Moved example to sol.27.4 2024 (SS3)

--- data.R
+++ data.R
@@ -19,13 +19,13 @@ cores <- 3
 source("utilities.R")
 
 # LOAD AAP SA results, 2022 ICES WGNSSK sol.27.4
-load('bootstrap/data/sol274.rda')
-
-# INTERMEDIATE year
-iy <- 2023
+load('boot/data/sol274.rda')
 
 # DATA year
-dy <- iy - 1
+dy <- dims(run)$maxyear
+
+# INTERMEDIATE year
+iy <- dy + 1
 
 # FINAL year
 fy <- 2042
@@ -33,7 +33,9 @@ fy <- 2042
 # NUMBER of iterations
 it <- 500
 
+# RNG seed
 set.seed(987)
+
 
 # - Stock-recruitment relationship(s) **
 
@@ -51,39 +53,33 @@ srpars <- bootstrapSR(run, iters=it,
 # SAVE
 save(fits, srpars, file="data/bootstrap.rda", compress="xz")
 
-
 # - CONSTRUCT OM
 
-# GENERATE future deviances: lognormal autocorrelated **
-srdevs <- rlnormar1(sdlog=srpars$sigmaR, rho=srpars$rho, years=seq(dy, fy))
+# GENERATE lognormal rec deviances, sigma and rho from SS3
+srdevs <- rlnormar1(n=500, sdlog=0.5384, rho=0, years=seq(dy - 10, fy))
 
-plot(srdevs)
+plot(srdevs) +
+  geom_vline(xintercept=2022, linetype=2)
 
-# BUILD FLom
-om <- FLom(stock=propagate(run, it), refpts=refpts, model='mixedsrr',
-  params=srpars, deviances=srdevs)
+# BUILD FLom w/ SRR
+om <- FLom(stock=propagate(run, it), refpts=refpts, model='bevholtss3',
+  params=params(srr), deviances=srdevs)
+
+# HINDCAST for last 10 years
+om <- fwd(om, catch=catch(om)[, ac(2013:2023)], sr=rec(om)[, ac(2013:2023)])
 
 # SETUP om future: average of last 3 years **
 om <- fwdWindow(om, end=fy)
 
-# SET stochastic rec dy
-rec(stock(om))[, '2022'] <- rec(om)[1, '2022'] * srdevs[, '2022']
+# PROJECT forward for iy assumption (TAC) **
+om <- fwd(om, catch=FLQuant(3675, dimnames=list(year=2024)))
 
-# PROJECT forward for iy assumption
-om <- fwd(om, catch=FLQuant(4289.2, dimnames=list(year=2023)))
+# TODO: ADD constant F
+om <- fwd(om, fbar=expand(fbar(run)[,'2023'], year=2024))
 
 # F and SSB deviances
 sdevs <- shortcut_devs(om, Fcv=0.212, Fphi=0.423, SSBcv=0.10)
 
-
-# - CONSTRUCT iem, implementation error module w/10% noise **
-
-iem <- FLiem(method=noise.iem,
-  args=list(noise=rlnorm(500, rec(om) %=% 0, 0.1)))
-
-
 # - SAVE
 
-save(om, iem, sdevs, file="data/data.rda", compress="xz")
-
-plan(sequential)
+save(om, sdevs, file="data/data.rda", compress="xz")
